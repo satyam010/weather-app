bf8f328cb5ae306aa234dd8533e2c8bdee195293
Satyam Prakash <prakashsatyam6@gmail.com>
2020-02-16 15:06:49 +0530

    Updates

---
+++ precache-manifest.4cbb7b56d3cca03e59069672b5971aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e4ee67148d2849a871c39c73c3ebdf",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9ef8e76e6c7f7fcff9c1",
+    "url": "/weather-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b595448a7786f105a734",
+    "url": "/weather-app/static/js/2.46e4d619.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app/static/js/2.46e4d619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef8e76e6c7f7fcff9c1",
+    "url": "/weather-app/static/js/main.d32beb3e.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
